30177a896ceab7bcd10d6ee4d79f20ec85747e0b
Joy George Kunjikkuru <joymon@gmail.com>
2020-09-21 13:49:53 -0400

    Added PowerShell to setup environment and install some apps

--- choco-powershell/readme.md
+++ choco-powershell/readme.md
@@ -4,6 +4,13 @@
 
 Web URL - Click [here to install using Boxstarter](http://boxstarter.org/package/url?https://raw.githubusercontent.com/joymon/win10-dev-box-setup/master/choco-powershell/setup.ps1)
 
+# What are the changes and software installed
+
+## Environment
+- On Power monitor timeout to 20 mins
+- On Power never sleep 
+## Software
+
 # References
 
 [https://edi.wang/post/2018/12/21/automate-windows-10-developer-machine-setup](https://edi.wang/post/2018/12/21/automate-windows-10-developer-machine-setup)
--- choco-powershell/setup.ps1
+++ choco-powershell/setup.ps1
@@ -1,5 +1,45 @@
+#### HELPER FUNCTIONS ####
+function Check-Command($cmdname) {
+    return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
+}
+#### HELPER FUNCTIONS ####
 Write-Host "Setting up power options"
 Powercfg /Change monitor-timeout-ac 20
 Powercfg /Change standby-timeout-ac 0
 Write-Host "Completed power options" -Foreground green
+Write-Host "Installing dev tools using choco"
+if (Check-Command -cmdname 'choco') {
+    Write-Host "Choco is already installed, skip installation."
+}
+else {
+    Write-Host "Installing Chocolatey first..."
+    Write-Host "------------------------------------" 
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    Write-Host "Installed Chocolatey" -ForegroundColor Green
+}
+$Apps = @(
+    "git",
+    "microsoft-edge",
+    "googlechrome",
+    "vlc",
+    "dotnetcore-sdk",
+    "wget",
+    #"vscode",
+    "sysinternals",
+    "notepadplusplus.install",
+    "fiddler",
+    "filezilla",
+    #"lightshot.install",
+    "microsoft-teams.install",
+    "teamviewer",
+    #"github-desktop",
+    #"irfanview",
+    "nodejs-lts",
+    "azure-cli",
+    "powershell-core")
+
+foreach ($app in $Apps) {
+    cinst $app -y
+} 
+Write-Host "Installed dev tools" -Foreground green
 
